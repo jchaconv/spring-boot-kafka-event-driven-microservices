5a96c4c73deb67d988d3f5dd7e8c66d4943b3196
Julio Chacon <julio_chacon@epamneoris.com>
2025-12-04 18:09:44 -0500

    feature/section9-kafka-consumer new email notification service scaffolding

---
+++ email-notification-microservice/src/test/java/com/vilelo/ws/emailnotification/EmailNotificationMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vilelo.ws.emailnotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailNotificationMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
