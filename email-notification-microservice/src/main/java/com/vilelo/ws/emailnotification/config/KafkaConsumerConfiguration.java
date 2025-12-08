662e6ede3c097325bc8390aba331dfc4894b7a2f
Julio Chacon <julio_chacon@epamneoris.com>
2025-12-08 16:39:41 -0500

    feature/section18-integration-testing-kafka-consumer section completed with consumer integration test succesful

--- email-notification-microservice/src/main/java/com/vilelo/ws/emailnotification/config/KafkaConsumerConfiguration.java
+++ email-notification-microservice/src/main/java/com/vilelo/ws/emailnotification/config/KafkaConsumerConfiguration.java
@@ -40,6 +40,8 @@ public class KafkaConsumerConfiguration {
         config.put(JacksonJsonDeserializer.TRUSTED_PACKAGES, environment.getProperty("spring.kafka.consumer.properties.spring.json.trusted.packages"));
         config.put(ConsumerConfig.GROUP_ID_CONFIG, environment.getProperty("spring.kafka.consumer.group-id"));
 
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, environment.getProperty("spring.kafka.consumer.auto-offset-reset"));
+
         return new DefaultKafkaConsumerFactory<>(config);
     }
 
--- email-notification-microservice/src/test/java/com/vilelo/ws/emailnotification/ProductCreatedEventHandlerIntegrationTest.java
+++ email-notification-microservice/src/test/java/com/vilelo/ws/emailnotification/ProductCreatedEventHandlerIntegrationTest.java
@@ -1,9 +1,101 @@
 package com.vilelo.ws.emailnotification;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.vilelo.ws.core.ProductCreatedEvent;
+import com.vilelo.ws.emailnotification.handler.ProductCreatedEventHandler;
+import com.vilelo.ws.emailnotification.io.ProcessedEventEntity;
+import com.vilelo.ws.emailnotification.io.ProcessedEventRepository;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.mockito.ArgumentCaptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.*;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
+import org.springframework.web.client.RestTemplate;
 
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+@EmbeddedKafka(partitions = 3)
+@SpringBootTest(properties = "spring.kafka.bootstrap-servers=${spring.embedded.kafka.brokers}")
 class ProductCreatedEventHandlerIntegrationTest {
 
+    @MockitoBean
+    ProcessedEventRepository processedEventRepository;
+
+    @MockitoBean
+    RestTemplate restTemplate;
+
+    @Autowired
+    KafkaTemplate<String, Object> kafkaTemplate;
+
+    @MockitoSpyBean
+    ProductCreatedEventHandler eventHandler;
+
+    @Autowired
+    private ProductCreatedEventHandler productCreatedEventHandler;
+
+    @Test
+    public void testProductCreatedEventHandler_OnProductCreated_HandlesEvent() throws Exception {
+
+        //Arrange
+        ProductCreatedEvent productCreatedEvent = new ProductCreatedEvent();
+        productCreatedEvent.setProductId(UUID.randomUUID().toString());
+        productCreatedEvent.setPrice(new BigDecimal(299));
+        productCreatedEvent.setQuantity(1);
+        productCreatedEvent.setTitle("Laptop HP EliteBook 845 G8");
+
+        String messageId = UUID.randomUUID().toString();
+        String messageKey = productCreatedEvent.getProductId();
+
+        ProducerRecord<String, Object> record = new ProducerRecord<>(
+                "product-created-events-topic",
+                messageKey,
+                productCreatedEvent
+        );
+
+        record.headers().add("messageId", messageId.getBytes());
+        record.headers().add(KafkaHeaders.RECEIVED_KEY, messageKey.getBytes());
+
+        ProcessedEventEntity processedEventEntity = new ProcessedEventEntity();
+        when(processedEventRepository.findByMessageId(anyString())).thenReturn(processedEventEntity);
+        when(processedEventRepository.save(any(ProcessedEventEntity.class))).thenReturn(null);
+
+        String responseBody = "{\"key\":\"value\"}";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        ResponseEntity<String> responseEntity = new ResponseEntity<>(responseBody, headers, HttpStatus.OK);
+
+        when(restTemplate.exchange(any(String.class), any(HttpMethod.class), isNull(), eq(String.class))).thenReturn(responseEntity);
+
+        //Act
+        kafkaTemplate.send(record).get();
+
+        //Assert
+        ArgumentCaptor<String> messageIdCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> messageKeyCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<ProductCreatedEvent> eventCaptor = ArgumentCaptor.forClass(ProductCreatedEvent.class);
+
+        verify(productCreatedEventHandler, timeout(5000).times(1)).handle(eventCaptor.capture(),
+                messageIdCaptor.capture(),
+                messageKeyCaptor.capture());
+
+        assertEquals(messageId, messageIdCaptor.getValue());
+        assertEquals(messageKey, messageKeyCaptor.getValue());
+        assertEquals(productCreatedEvent.getProductId(), eventCaptor.getValue().getProductId());
+
+    }
 
 
 
